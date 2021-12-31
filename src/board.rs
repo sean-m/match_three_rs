03b1817d3a3e9b95c65ef2921e28cfebd172aae1
Sean McArdle <sean@mcardletech.com>
2021-12-30 17:19:24 -0800

    Working on board.

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,6 @@
 
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 pub enum Facet {
 	None,
 	Red,
@@ -9,49 +11,95 @@ pub enum Facet {
 	Circle,
 }
 
-impl Clone for Facet {
-	// add code here
-	fn clone(&self) -> Self { 
-		let temp = *self;
-		temp
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Piece {
+	facet1: Facet,
+	facet2: Facet,
+	facet3: Facet,
+}
+
+impl Piece {
+	pub fn empty () -> Piece {
+		Piece {
+			facet1: Facet::None,
+			facet2: Facet::None,
+			facet3: Facet::None,
+		}
 	}
 }
 
-impl Copy for Facet { }
-
-pub struct Piece {
-	facets: (Facet, Facet, Facet),
-}
-
-impl Clone for Piece {
-	fn clone(&self) -> Self { Piece { facets: self.facets.clone() } }
-}
 
 pub struct GameBoard {
-	pub width: u8,
-	pub height: u8,
+	pub width: usize,
+	pub height: usize,
 	pub pieces: Vec<Piece>,
 }
 
 impl GameBoard {
-	pub fn new (width: u8, height: u8) -> GameBoard {
+	pub fn new (width: usize, height: usize) -> GameBoard {
 		GameBoard {
-			width,
-			height,
-			pieces: vec![Piece{ facets: (Facet::None, Facet::None, Facet::None)}; (width * height).into()]
+			width: width.into(),
+			height: height.into(),
+			pieces: vec![Piece::empty(); (width * height).into()]
 		}
 	}
 
 	pub fn get_size (&self) -> usize {
 		self.pieces.len()
 	}
+
+	pub fn get_used_spaces(&self) -> usize {
+		self.pieces.iter().filter(|&x| *x != Piece::empty()).count()
+	}
+
+	pub fn fill_board(&self) {
+		while self.get_used_spaces() == 0 {
+			for column in 0.. self.width-1 {
+				self.drop_new_piece(column);
+			}
+		}
+	}
+
+	fn drop_new_piece(&self, column: usize) {
+		// Place at top
+		if self.spot_empty(0, column) {
+
+		}
+
+		// Fall down
+		for row in 1..self.height-1 {
+			if self.spot_empty(row, column) {
+
+			}
+		}
+	}
+
+	fn spot_empty(&self, x: usize, y: usize) -> bool {
+
+		true
+	}
 }
+
 
 mod tests{
 	use crate::board::GameBoard;
-	
+
 	#[test]
 	fn board_size_correct() {
 		assert_eq!(GameBoard::new(8,8).get_size(), 64);
+	}
+
+	#[test]
+	fn empty_board_used_spaces_eq_zero() {
+		let board = GameBoard::new(8,8);
+		assert_eq!(board.get_used_spaces(), 0);
+	}
+
+	#[test]
+	fn filled_board_is_used_up() {
+		let board = GameBoard::new(8,8);
+		board.fill_board();
+		assert_eq!(board.get_used_spaces(), 64);
 	}
 }
