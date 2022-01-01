6faa977d69b854c999ef35f6b597f2172aac0bea
Sean McArdle <sean@mcardletech.com>
2022-01-01 10:28:00 -0800

    Switched to using strings for facets so the token values can be defined by the game code rather than the board.

--- src/board.rs
+++ src/board.rs
@@ -4,7 +4,7 @@
 pub enum Facet {
 	None,
 	Red,
-	Grean,
+	Green,
 	Blue,
 	Square,
 	Diamond,
@@ -12,24 +12,36 @@ pub enum Facet {
 }
 
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Piece {
-	facet1: Facet,
-	facet2: Facet,
-	facet3: Facet,
+	facet1: String,
+	facet2: String,
+	facet3: String,
 }
 
 impl Piece {
 	pub fn empty () -> Piece {
 		Piece {
-			facet1: Facet::None,
-			facet2: Facet::None,
-			facet3: Facet::None,
+			facet1: String::from(""),
+			facet2: String::from(""),
+			facet3: String::from(""),
+		}
+	}
+
+	pub fn new (
+		facet_val1: &str,
+		facet_val2: &str,
+		facet_val3: &str,
+		) -> Piece {
+		Piece {
+			facet1: String::from(facet_val1),
+			facet2: String::from(facet_val2),
+			facet3: String::from(facet_val3),
 		}
 	}
 }
 
-
+#[derive(Debug, Clone, PartialEq)]
 pub struct GameBoard {
 	pub width: usize,
 	pub height: usize,
@@ -53,37 +65,51 @@ impl GameBoard {
 		self.pieces.iter().filter(|&x| *x != Piece::empty()).count()
 	}
 
-	pub fn fill_board(&self) {
-		while self.get_used_spaces() == 0 {
-			for column in 0.. self.width-1 {
-				self.drop_new_piece(column);
-			}
-		}
+	// TODO put this somewhere else, the board doesn't need to know how to fill itself. That's a function of the game.
+	// pub fn fill_board(&mut self) {
+	// 	while self.get_used_spaces() == 0 {
+	// 		for column in 0.. self.width-1 {
+	// 			self.drop_new_piece(column);
+	// 		}
+	// 	}
+	// }
+
+	// TODO put this somewhere else, the board doesn't need to know how to fill itself. That's a function of the game.
+	// fn drop_new_piece(&mut self, column: usize) {
+	// 	// Place at top
+	// 	if self.spot_empty(0, column) {
+	// 		let token = Piece{ 
+	// 			facet1: String::from("G"),
+	// 			facet2: String::from(""),
+	// 			facet3: String::from(""),
+	// 		};
+	// 		//TODO Make this select random facets
+	// 		//TODO Make randomness depend on gmae rules
+	// 		self.place_token(token, 0, column);
+	// 	}
+	// }
+
+	fn spot_empty(&self, row: usize, column: usize) -> bool {
+		let offset = self.width * row;
+		let offset = offset + column - 1;
+		self.pieces[offset] == Piece::empty()
 	}
 
-	fn drop_new_piece(&self, column: usize) {
-		// Place at top
-		if self.spot_empty(0, column) {
-
+	pub fn place_token(&mut self, token: Piece, row: usize, column: usize) -> bool {
+		if 0 >= row || 0 >= column 
+		|| row > self.height || column > self.width {
+			return false
 		}
 
-		// Fall down
-		for row in 1..self.height-1 {
-			if self.spot_empty(row, column) {
-
-			}
-		}
-	}
-
-	fn spot_empty(&self, x: usize, y: usize) -> bool {
-
+		let offset = self.width * (row - 1);
+		let offset = offset + column - 1;
+		self.pieces[offset] = token;
 		true
 	}
 }
 
-
 mod tests{
-	use crate::board::GameBoard;
+	use crate::board::*;
 
 	#[test]
 	fn board_size_correct() {
@@ -97,9 +123,47 @@ mod tests{
 	}
 
 	#[test]
+	fn placing_token_uses_spaces() {
+		let mut board = GameBoard::new(8,8);
+		let token = Piece::new("G","","");
+
+		let before = board.get_used_spaces();
+		board.place_token(token, 1, 1);
+		
+		assert_ne!(before, board.get_used_spaces());
+	}
+
+	#[test]
+	fn placed_token_is_no_longer_empty() {
+		let mut board = GameBoard::new(8,8);
+		let token = Piece::new("G","","");
+
+		let before = board.spot_empty(1,1);
+		board.place_token(token, 1, 1);
+		let after = board.spot_empty(1,1);
+		
+		assert_ne!(before, after)
+	}
+
+	/*
+	#[test]
 	fn filled_board_is_used_up() {
 		let board = GameBoard::new(8,8);
 		board.fill_board();
 		assert_eq!(board.get_used_spaces(), 64);
+	}
+	*/
+
+	#[test]
+	fn wont_place_tokens_out_of_bounds() {
+		let mut board = GameBoard::new(8,8);
+		let token = Piece::new("G","","");
+		let token2 = Piece::new("G","","");
+
+		let test1 = board.place_token(token, 9, 1);
+		let test2 = board.place_token(token2, 1, 1);
+
+		assert_eq!(false, test1);
+		assert_eq!(true, test2);
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,16 @@ mod board;
 
 fn main() {
     
-    let board = board::GameBoard::new(8, 8);
+    let mut board = board::GameBoard::new(4, 2);
+    let token = board::Piece::new("G","","");
+    board.place_token(token,1,2);
+
+    let token2 = board::Piece::new("G","","");
+    board.place_token(token2,2,1);
+
+    let token2 = board::Piece::new("R","","");
+    board.place_token(token2,2,3);
 
     println!("Board {} by {} is {} big.", board.width, board.height, board.get_size());
+    println!("{:#?}", board);
 }
